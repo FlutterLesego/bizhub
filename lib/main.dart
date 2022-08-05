e0bbbf7b15d13c9287a3e76abd58dd12241e9304
Lesego Rabanye <lesegorabanye@gmail.com>
2022-08-05 06:52:43 +0200

    skeleton commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firstapp/routes/route_manager.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteManager.onGenerateRoute,
+      initialRoute: RouteManager.loadingPage,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      
+    );
+  }
+}
+
+
---
+++ lib/widgets/login_form.dart
@@ -0,0 +1,101 @@
+import 'package:firstapp/routes/route_manager.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../misc/constants.dart';
+import '../misc/validators.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBoxH30(),
+          TextFormField(
+            textInputAction: TextInputAction.next,
+            validator: validateEmail,
+            controller: emailController,
+            decoration: formDecoration(
+              'Email address',
+              Icons.mail_outline,
+            ),
+          ),
+          const SizedBoxH10(),
+          TextFormField(
+            textInputAction: TextInputAction.done,
+            validator: validatePassword,
+            controller: passwordController,
+            decoration: formDecoration(
+              'Password',
+              Icons.lock_outline,
+            ),
+          ),
+          const SizedBoxH10(),
+          //forgot password
+          ButtonBar(
+            alignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                  onPressed: () {
+                  },
+                  child: const Text(
+                    "Forgot Password?",
+                    style: style16Black,
+                  ))
+            ],
+          ),
+          //SIGN IN BUTTON
+          CupertinoButton(
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+              color: Colors.indigo,
+              child: const Text(
+                'Sign in',
+                style: style16White,
+              ),
+              onPressed: () {}),
+          const SizedBoxH20(),
+          CupertinoButton(
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+              color: Colors.white,
+              child: const Text(
+                'Sign up',
+                style: style16Indigo,
+              ),
+              onPressed: () {
+                    Navigator.popAndPushNamed(
+                        context, RouteManager.registerPage);
+              })
+        ],
+      ),
+    ));
+  }
+}
